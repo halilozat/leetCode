cf4c4fa16bfb681675879a601d673d589f6da95a
halilozat <halilozat@hotmail.com>
2021-06-16 16:41:45 +0300

    roman to integer is solved

---
+++ romantToInteger/romanToInteger.js
@@ -0,0 +1,34 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+ var romanToInt = (s) => {
+    
+    const symbols = { 
+         'I': 1,
+         'V': 5,
+         'X': 10,
+         'L': 50,
+         'C': 100,
+         'D': 500,
+         'M': 1000
+     }
+ 
+     let result = 0;
+ 
+     for (i=0; i < s.length; i++){
+         
+         const cur = symbols[s[i]];
+         const next = symbols[s[i+1]];
+         
+         if (cur < next){
+             result += next - cur // IV -> 5 - 1 = 4
+             i++
+         } else {
+             result += cur // V -> 5
+         }
+     }
+ 
+     return result; 
+     
+ };
